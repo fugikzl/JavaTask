57246300f6d4df8bb80306c1785484c831cb06a8
fugikzl <sailaubek.nar@gmail.com>
2023-02-02 18:15:51 +0600

    initial

---
+++ PlaneComponent/PlaneComponent.java
@@ -0,0 +1,16 @@
+package PlaneComponent;
+public abstract class PlaneComponent {
+    private int weight;
+
+    private int volume;
+
+    public int getWeight()
+    {
+        return this.weight;
+    }
+
+    public int getVolume()
+    {
+        return this.volume;
+    }
+}
