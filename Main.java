4a712afe12aa41f5fa9e71145a32ba9b1c392fd8
meiramura <121001535+meiramura@users.noreply.github.com>
2023-02-15 12:15:45 +0600

    Update Main.java
    Upgraded Main.java with adding the input and output

--- Main.java
+++ Main.java
@@ -2,15 +2,75 @@ import PlaneComponent.Wing.Wing;
 import PlaneComponent.Engine.Engine;
 import PlaneComponent.Frame.Frame;
 
+import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws Exception {
-        Engine engine = new Engine("test", 45000, 450, 450);
-        Wing wing = new Wing("testWing", 0.869, 600, 1000);
-        Frame frame = new Frame("testFrame", 0.45, 1200, 1450);
-        
+        Scanner sc = new Scanner(System.in);
+
+        // Input parameters for Engine class
+        System.out.println("Enter parameters for Engine:");
+        System.out.print("Name: ");
+        String engineName = sc.nextLine();
+        System.out.print("Power: ");
+        int enginePower = sc.nextInt();
+        System.out.print("Weight: ");
+        int engineWeight = sc.nextInt();
+        System.out.print("Volume: ");
+        int engineVolume = sc.nextInt();
+        Engine engine = new Engine(engineName, enginePower, engineWeight, engineVolume);
+        sc.nextLine(); // Consume leftover newline character
+
+        // Input parameters for Wing class
+        System.out.println("Enter parameters for Wing:");
+        System.out.print("Name: ");
+        String wingName = sc.nextLine();
+
+        float wingAirResistance;
+        while (true) {
+            System.out.print("Air resistance coefficient: ");
+            String input = sc.nextLine();
+            try {
+                wingAirResistance = Float.parseFloat(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid numeric value.");
+            }
+        }
+
+        System.out.print("Weight: ");
+        int wingWeight = sc.nextInt();
+        System.out.print("Volume: ");
+        int wingVolume = sc.nextInt();
+        Wing wing = new Wing(wingName, wingAirResistance, wingWeight, wingVolume);
+        sc.nextLine(); // Consume leftover newline character
+
+        // Input parameters for Frame class
+        System.out.println("Enter parameters for Frame:");
+        System.out.print("Name: ");
+        String frameName = sc.nextLine();
+        float frameAirResistance;
+        while (true) {
+            System.out.print("Air resistance coefficient: ");
+            String input = sc.nextLine();
+            try {
+                frameAirResistance = Float.parseFloat(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid numeric value.");
+            }
+        }
+        System.out.print("Weight: ");
+        int frameWeight = sc.nextInt();
+        System.out.print("Capacity: ");
+        int frameCapacity = sc.nextInt();
+        Frame frame = new Frame(frameName, frameAirResistance, frameWeight, frameCapacity);
+        sc.nextLine(); // Consume leftover newline character
+
+        // Create Plane object with input parameters
         Plane plane = new Plane(engine, frame, wing);
 
+        // Output plane information
         plane.getPlaneInfo();
     }
 }
